47f585df97ad78ee37bd6fd549fea5b5561aecbf
zakaria-magdy2 <zekomagdy613@gmail.com>
2026-02-21 16:52:07 +0200

    Add files via upload

---
+++ js/statistics.js
@@ -0,0 +1,83 @@
+// جدول الهدافين
+$.ajax({
+  url: "http://football-backend-production-8b1f.up.railway.app/laliga-topscorers",
+  dataType: "json",
+  type: "GET",
+  success: function (data) {
+    fetchTopScorers(data);
+  },
+  error: function (err) {
+    console.error("Error fetching top scorers:", err);
+  },
+});
+
+function fetchTopScorers(data) {
+  for (var i = 0; i < data.length; i++) {
+    var row = `
+      <tr>
+        <td>${data[i].rank}</td>
+        <td>${data[i].name}</td>
+        <td>${data[i].team}</td>
+        <td>${data[i].goals}</td>
+        <td>${data[i].assists}</td>
+        <td>${data[i].penalties}</td>
+      </tr>
+    `;
+
+    $("#topScores tbody").append(row);
+  }
+}
+
+// جدول أقوى هجوم
+$.ajax({
+  url: "http://football-backend-production-8b1f.up.railway.app/laliga-attack",
+  dataType: "json",
+  type: "GET",
+  success: function (data) {
+    fetchBestAttack(data.bestAttack);
+  },
+  error: function (err) {
+    console.log(err);
+  },
+});
+
+function fetchBestAttack(data) {
+  for (var i = 0; i < data.length; i++) {
+    var row = `
+           <tr>
+           <td>${data[i].rank}</td>
+           <td>${data[i].team}</td>
+           <td>${data[i].goals}</td>
+           </tr>
+           `;
+
+    $("#best-attack tbody").append(row);
+  }
+}
+
+// اقوي دفاع
+$.ajax({
+  url: `http://football-backend-production-8b1f.up.railway.app/laliga-defense`,
+  dataType: "json",
+  type: "GET",
+  success: function (data) {
+    fetchBestDefense(data.bestDefense);
+  },
+  error: function (err) {
+    console.log(err);
+  },
+});
+
+function fetchBestDefense(data) {
+  for (var i = 0; i < data.length; i++) {
+    var row = `
+    <tr>
+    <td>${data[i].rank}</td>
+    <td> ${data[i].team}</td>
+    <td>${data[i].conceded}</td>
+    </tr>
+             `;
+
+    $("#best-defense tbody").append(row);
+  }
+}
